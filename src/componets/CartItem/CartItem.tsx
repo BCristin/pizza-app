ed055d0863f5f4cb874b2e66d8f0b174ea6e4c4f
Cristin <tianncristiann@gmail.com>
2023-10-15 06:39:39 +0300

    L122

--- src/componets/CartItem/CartItem.tsx
+++ src/componets/CartItem/CartItem.tsx
@@ -22,22 +22,23 @@ export const CartItem = (props: CartItemProps) => {
 	return (
 		<div className={styles.item}>
 			<div className={styles.image} style={{ backgroundImage: `url('${props.image}')` }}>
-				<div className={styles.description}>
-					<div className={styles.name}>{props.name}</div>
-					<div className={styles.price}>{props.price}&nbsp;₽</div>
-				</div>
-				<div className={styles.actions}>
-					<button className={styles.minus} onClick={descrease}>
-						<img src="/minus-icon.svg" alt="delete" />
-					</button>
-					<div className={styles.number}>{props.count}</div>
-					<button className={styles.plus} onClick={increase}>
-						<img src="/plus-icon.svg" alt="add" />
-					</button>
-					<button className={styles.remove} onClick={remove}>
-						<img src="/delete-icon.svg" alt="delete all" />
-					</button>
-				</div>
+				{' '}
+			</div>
+			<div className={styles.description}>
+				<div className={styles.name}>{props.name}</div>
+				<div className={styles.price}>{props.price}&nbsp;₽</div>
+			</div>
+			<div className={styles.actions}>
+				<button className={styles.minus} onClick={descrease}>
+					<img src="/minus-icon.svg" alt="Удалить из корзины" />
+				</button>
+				<div className={styles.number}>{props.count}</div>
+				<button className={styles.plus} onClick={increase}>
+					<img src="/plus-icon.svg" alt="Добавить в корзину" />
+				</button>
+				<button className={styles.remove} onClick={remove}>
+					<img src="/delete-icon.svg" alt="Удалить все" />
+				</button>
 			</div>
 		</div>
 	);
